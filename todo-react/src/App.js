08b1fe42086675050d8a3765bec0f1299ea6c29d
Amrut Sagar <amrutsagar06@gmail.com>
2022-01-26 23:18:30 +0530

    react todo

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React , { useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 //importing components.
 import Form from './components/Form';
 
+import TodoList from './components/TodoList';
 function App() {
+  const [inputText,setInputText] = useState("");
   return (
     <div className="App">
-      <header> <h1>AMT's ToDo List</h1></header>
-     <Form/>
+      <header> <h1>AMT's ToDo List {inputText}</h1></header>
+     <Form setInputText={setInputText}/>
+     <TodoList/>
     </div>
   );
 }
