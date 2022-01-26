445d3611291fb043c59ad77c879f4e990395bb7a
Amrut Sagar <amrutsagar06@gmail.com>
2022-01-26 21:49:06 +0530

    todo react

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+//importing components.
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello tooodooo react</h1>
+      <header> <h1>AMT's ToDo List</h1></header>
+     <Form/>
     </div>
   );
 }
